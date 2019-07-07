300f6d5b37316856a79f85d6d37eb57d38bd0582
Leon Nikitin <nlv@lab321.ru>
2019-07-07 21:54:42 +0600

    скомпилировали демо

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Lib
-    ( someFunc
+    ( proxy
     ) where
+
+import Network.HTTP.ReverseProxy
+import Data.Conduit.Network
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+proxy :: IO ()
+proxy = runTCPServer (serverSettings 3000 "*") $ \appData ->
+    rawProxyTo
+        (\_headers -> return $ Right $ ProxyDest "www.example.com" 80)
+        appData
